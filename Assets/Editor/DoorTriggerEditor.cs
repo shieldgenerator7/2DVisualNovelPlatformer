c6e17c2b40da1118a2be81f2659126cf92d48d06
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-30 18:22:29 -0500

    Add tools*
    *Copied from Party Witch ("Grimm's Wish")

---
+++ Assets/Editor/DoorTriggerEditor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DoorTrigger))]
+[CanEditMultipleObjects]
+public class DoorTriggerEditor : EventTriggerEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        //Connect button
+        if (targets.Length == 2)
+        {
+            if (GUILayout.Button("Connect"))
+            {
+                DoorTrigger door1 = (DoorTrigger)targets[0];
+                DoorTrigger door2 = (DoorTrigger)targets[1];
+                Undo.RecordObjects(
+                    targets,
+                    "Connect " + door1.name + " and " + door2.name + " together."
+                    );
+                door1.connectTo(door2);
+                door2.connectTo(door1);
+            }
+        }
+        else
+        {
+            GUILayout.Box("Select two DoorTriggers for more options.");
+        }
+    }    
+}
---
+++ Assets/Editor/EventTriggerEditor.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EventTrigger), true)]
+[CanEditMultipleObjects()]
+public class EventTriggerEditor : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        List<EventTrigger> triggers = targets.ToList()
+            .Cast<EventTrigger>().ToList();
+        //Assign Ids
+        if (GUILayout.Button("Assign Id"))
+        {
+            assignIds();
+        }
+
+        //Conversion options
+        //If they're all of the right type(s),
+        if (triggers.TrueForAll(
+            t => t is DialogueTrigger || t is ItemCollectTrigger
+            ))
+        {
+            if (triggers.Any(t => t is ItemCollectTrigger))
+            {
+                //Convert to DialogueTrigger
+                if (GUILayout.Button("Convert to DialogueTrigger"))
+                {
+                    List<ItemCollectTrigger> converts = triggers.Where(
+                        t => !(t is DialogueTrigger)
+                        ).Cast<ItemCollectTrigger>().ToList();
+                    Undo.RecordObjects(
+                        converts.ToArray(),
+                        "Convert " + converts.Count + " to DialogueTriggers"
+                        );
+                    converts.ForEach(
+                        t =>
+                        {
+                            DialogueTrigger dialogue = t.gameObject.AddComponent<DialogueTrigger>();
+                            dialogue.title = t.title;
+                            dialogue.id = t.id;
+                            DestroyImmediate(t);
+                            EditorUtility.SetDirty(dialogue);
+                        }
+                        );
+                    Debug.Log("Converted " + converts.Count + " EventTriggers to DialogueTrigger");
+                }
+            }
+            if (triggers.Any(t => t is DialogueTrigger))
+            {
+                //Convert to ItemCollectTrigger
+                if (GUILayout.Button("Convert to ItemCollectTrigger"))
+                {
+                    List<DialogueTrigger> converts = triggers.Where(
+                        t => !(t is ItemCollectTrigger)
+                        ).Cast<DialogueTrigger>().ToList();
+                    Undo.RecordObjects(
+                        converts.ToArray(),
+                        "Convert " + converts.Count + " to ItemCollectTriggers"
+                        );
+                    converts.ForEach(
+                        t =>
+                        {
+                            ItemCollectTrigger item = t.gameObject.AddComponent<ItemCollectTrigger>();
+                            item.title = t.title;
+                            item.id = t.id;
+                            DestroyImmediate(t);
+                            EditorUtility.SetDirty(item);
+                        }
+                        );
+                    Debug.Log("Converted " + converts.Count + " EventTriggers to ItemCollectTriggers");
+                }
+            }
+        }
+        else if (triggers.Any(
+            t => t is DialogueTrigger || t is ItemCollectTrigger
+            ))
+        {
+            GUILayout.Box("Can only convert between DialogueTrigger and ItemCollectTrigger.");
+        }
+    }
+
+    private void assignIds()
+    {
+        List<EventTrigger> triggers = targets.Cast<EventTrigger>().ToList();
+        Undo.RecordObjects(triggers.ToArray(), "Assign Ids to EventTrigger objects.");
+        //Unset ids to allow potential for first id to be 0
+        triggers.ForEach(t => t.id = -1);
+        //Find the highest number among available ids
+        int maxKnownId = FindObjectsOfType<EventTrigger>().Max(t => t.id);
+        //Assign unique ids
+        triggers.ForEach(
+            t =>
+            {
+                t.id = maxKnownId + 1;
+                maxKnownId++;
+            }
+            );
+    }
+}
