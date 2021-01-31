ddf4e141dfcb58814f50c0db68db5b8e7b061028
LordBAP <lordbap@gmail.com>
2021-01-31 01:48:48 -0500

    More enemy stuff!

--- Assets/Enemies/TrapFired.cs
+++ Assets/Enemies/TrapFired.cs
@@ -4,30 +4,47 @@ using UnityEngine;
 
 public class TrapFired : MonoBehaviour
 {
-    public float myTTL = 3;
-    public Vector3 myVelocity = new Vector3 (0,3,0);
-    public float myX = 5;
+    public float myTTL = 2;
+    public Vector3 myVelocity = new Vector3 (5,5,0);
+    public float mySpeed = -5;
     public Rigidbody myRB;
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = this.gameObject.GetComponent<Rigidbody>();
-        Ray playerCheck = new Ray(transform.position, Vector3.right);
         RaycastHit inView;
+        
+
+        Ray playerCheck = new Ray(transform.position, Vector3.right);
         if (Physics.Raycast(playerCheck, out inView))
         {
             // look for 'player'
             if (inView.transform.tag == "Player")
             {
-                myX = myX * -1;
-                
+                Debug.Log(inView.distance);
+                myVelocity.x = inView.distance;
             }
+
             
-            myVelocity.x = myX;
-         //   myRB.velocity = myX;
-            Debug.Log(myRB);
         }
+        
+        playerCheck = new Ray(transform.position, Vector3.left);
+        if (Physics.Raycast(playerCheck, out inView))
+        {
+            // look for 'player'
+            if (inView.transform.tag == "Player")
+            {
+
+                myVelocity.x = inView.distance*-1;
+            }
+
+            
+        }
+
+        myVelocity.y = 6;
+        myRB.velocity = myVelocity;
+        Debug.Log(myRB);
     }
 
     // Update is called once per frame
